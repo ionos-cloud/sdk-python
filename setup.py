ab4fd1468256a8853e97a2f3de5574fb38eef1d4
Alexandru Toma <alexandru.toma@ionos.com>
2020-10-12 17:04:09 +0300

    fix doc url

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
          'Topic :: Software Development :: Libraries :: Application Frameworks',
          'Topic :: Internet :: WWW/HTTP'],
     project_urls={
-        'Documentation': 'https://github.com/ionos-cloud/ionos-cloud-sdk-python/blob/master/README.md',
+        'Documentation': 'https://github.com/ionos-cloud/ionos-cloud-sdk-python/blob/master/DOCS.md',
         'Source Code': 'https://github.com/ionos-cloud/ionos-cloud-sdk-python'
     }
 )
